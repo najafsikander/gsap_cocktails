38b076db27deca0ecaaf541c1950191aab4352ef
najafsikander <najafsikander@yahoo.com>
2025-07-08 12:13:07 +0400

    Added video scroll animation in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import {useGSAP} from "@gsap/react";
 import {SplitText} from "gsap/SplitText";
 import gsap from "gsap";
+import {useRef} from "react";
+import {useMediaQuery} from "react-responsive";
 const Hero = () => {
+
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth: 767});
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {type:'chars, words'})
         const paragraphSplit = new SplitText('.subtitle', {type:'lines'})
@@ -31,6 +37,24 @@ const Hero = () => {
             }
         }).to('.right-leaf', {y:400}, 0)
             .to('.left-leaf', {y:-400}, 0)
+        const startValue = isMobile?"top 50%":"center 60%";
+        const endValue = isMobile?"120% top":"bottom top";
+
+        let tl = gsap.timeline({
+            scrollTrigger:{
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            })
+        }
     },[])
     return (
         <>
@@ -54,6 +78,9 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+            <div className="video absolute inset-0">
+                <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload="auto"/>
+            </div>
         </>
     )
 }
